15817e4e969abd485f3ee9a523b70af0af74260e
bdiehr <btdiehr@gmail.com>
2015-12-29 00:18:37 -0500

    Improved documentation on markov chains; enfore subreddits to be lowercase; removed seed files

--- server/src/config/cronJobs.js
+++ server/src/config/cronJobs.js
@@ -1,7 +1,6 @@
 import debug from 'debug';
 import { CronJob } from 'cron';
 import analyzeTopPosts from '../tasks/analyzeTopPosts';
-import redditMarkovChain from '../tasks/redditMarkovChain';
 
 function cronJobConfig(app) {
   debug('dev')('Initiating Cron Jobs');
--- server/src/tasks/redditMarkovChain.js
+++ server/src/tasks/redditMarkovChain.js
@@ -7,30 +7,28 @@ import getTopStoriesReddit from '../services/getTopStoriesReddit';
 
 function redditMarkovChainClosure(subreddit) {
   return co.wrap(function * redditMarkovChain() {
-    const topRedditPosts = (yield getTopStoriesReddit(25, subreddit))
+    const lowerCaseSubreddit = subreddit.toLowerCase();
+
+    /** Fetch top reddit posts */
+    const topRedditPosts = (yield getTopStoriesReddit(25, lowerCaseSubreddit))
       .data.children.map(child => child.data);
 
     /** Ensure that the subreddit exists */
     if (topRedditPosts.length === 0) throw new Error('No such subreddit');
 
+    /** Fetch text to input into Markov Chain */
     const numberOfPosts = 100;
-    /** Fetch Reddit data */
-    const redditTitleTxt = (yield getTopStoriesReddit(numberOfPosts, subreddit))
+    const redditTitleTxt = (yield getTopStoriesReddit(numberOfPosts, lowerCaseSubreddit))
       .data.children.map(child => child.data)
       .map(post => post.title)
       .reduce((a, b) => `${a}\n${b}`, '');
 
-    const redditTitleSeedPath = path.resolve(__dirname, '..', `static/reddit_markov_chain/${subreddit}_seed.txt`);
-    yield new Promise((resolve, reject) => {
-      fs.appendFile(redditTitleSeedPath, redditTitleTxt, (err, success) => {
-        if (err) reject(err);
-        else resolve(success);
-      });
-    });
+    debug('dev')(`Completed scraping ${numberOfPosts} new reddit posts for ${lowerCaseSubreddit}`);
 
-    debug('dev')(`Completed scraping ${numberOfPosts} new reddit posts fro ${subreddit}`);
+    /** Feed text into Markov Chain */
     const quotes = new MarkovChain(redditTitleTxt);
 
+    /** Generate weird reddit posts */
     const weirdRedditPosts = topRedditPosts.map(post => {
       const seedWord = post.title.split(' ')[0];
       const minWords = 3;
@@ -43,7 +41,7 @@ function redditMarkovChainClosure(subreddit) {
       };
     });
 
-    const weirdRedditPostsPath = path.resolve(__dirname, '..', `static/reddit_markov_chain/${subreddit}`);
+    const weirdRedditPostsPath = path.resolve(__dirname, '..', `static/reddit_markov_chain/${lowerCaseSubreddit}`);
     yield new Promise((resolve, reject) => {
       fs.writeFile(weirdRedditPostsPath, JSON.stringify(weirdRedditPosts), (err, success) => {
         if (err) reject(err);
